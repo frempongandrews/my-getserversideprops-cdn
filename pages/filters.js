0bb7844aadd491837ea0210e85b0f460cd65d133
Andrews Frempong <frempongandrews@yahoo.com>
2021-07-29 12:18:00 +0100

    removed quotes

--- pages/filters.js
+++ pages/filters.js
@@ -32,8 +32,8 @@ const Filters = (props) => {
                 <h3>response header set to Cache-Control, s-maxage=20, stale-while-revalidate=180</h3>
                 <ul>
                     <li>If next request made within the next 20 seconds (s-maxage value), the first response, previously cached, will still be considered fresh and will be served</li>
-                    <li>If request is made between 20 seconds and 180 seconds(stale-while-revalidate value), the previous cached response will be considered "stale" and will be used to fulfill the API request. At the same time, "in the background," a revalidation request will be made to populate the cache with a fresh value for future use. </li>
-                    <li>If request is made after 180 seconds (stale-while-revalidate) then the stale response isn't used at all, and both fulfilling the browser's request and the cache revalidation will depend on getting a response back from the network</li>
+                    <li>If request is made between 20 seconds and 180 seconds(stale-while-revalidate value), the previous cached response will be considered stale and will be used to fulfill the API request. At the same time, in the background, a revalidation request will be made to populate the cache with a fresh value for future use. </li>
+                    <li>If request is made after 180 seconds (stale-while-revalidate) then the stale response isnt used at all, and both fulfilling the browsers request and the cache revalidation will depend on getting a response back from the network</li>
                 </ul>
             </div>
 
